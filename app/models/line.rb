dd03790f47149419a2dafd8c667fb05764c77796
Jan Kus <jan@railslove.com>
2012-04-11 21:25:55 +0200

    fixed a habtm relation

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,6 +1,7 @@
 class Line < ActiveRecord::Base
 
-  has_and_belongs_to_many :stations, :destinations
+  has_and_belongs_to_many :stations
+  has_and_belongs_to_many :destinations
 
   validates_presence_of :number, :kind
 end
