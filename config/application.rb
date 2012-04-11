e3b4a99da31a619cb69f3cf760a8601d71f3459c
Jan Kus <jan@railslove.com>
2012-04-11 21:39:16 +0200

    precompile files on production

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,8 @@ end
 
 module KvbApi
   class Application < Rails::Application
-    
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
