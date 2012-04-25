67ea253103c2c6cbc32f9058eeb4682b330fe1ab
rubiii <me@rubiii.com>
2012-04-25 18:40:37 +0200

    added lines

---
+++ lib/lines.rb
@@ -0,0 +1,31 @@
+require "open-uri"
+require "nokogiri"
+
+Lines = Struct.new(:line_id) do
+
+  NBSP = Nokogiri::HTML("&nbsp;").text
+
+  def call
+    table.children.each_with_object([]) do |col, memo|
+      link = col.child.child
+      next unless link["href"]
+
+      station_id   = link["href"].split("/").last.to_i
+      station_name = link.content
+
+      memo << { station_id: station_id, station_name: station_name }
+    end
+  end
+
+private
+
+  def table
+    response = open("http://www.kvb-koeln.de/german/hst/showline/0/#{line_id}/")
+    Nokogiri::HTML.parse(response).css("#content table")
+  end
+
+  def clean_content(node)
+    node.content.gsub(NBSP, "")
+  end
+
+end
