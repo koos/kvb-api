283ec521235c9305aa1c7c1bc9e965b6b1c54b8e
Daniel Lehmann <leh@l3h.de>
2012-04-28 20:01:52 +0200

    Added a skip list for the fetching of arrivals for stations which don't have their data up on kvb.de (like Bonn West)

--- lib/arrivals.rb
+++ lib/arrivals.rb
@@ -3,15 +3,22 @@ require "nokogiri"
 
 Arrivals = Struct.new(:station_id) do
 
+  # These stations don't seem to have data on kvb.de
+  SKIP_LIST = [683, 684, 685, 686, 688, 693, 694, 680, 743, 740, 741, 742, 134, 108, 107, 689, 696, 697, 681, 628, 739, 737, 736, 735, 734, 738, 731, 733, 732, 730]
+
   NBSP = Nokogiri::HTML("&nbsp;").text
 
   def import
-    @arrivals = table.children.inject([]) do |memo, col|
-      line        = clean_content(col.children[0])
-      destination = clean_content(col.children[1])
-      arrival     = clean_content(col.children[2]).to_i
+    if SKIP_LIST.include? station_id
+      @arrivals = []
+    else
+      @arrivals = table.children.inject([]) do |memo, col|
+        line        = clean_content(col.children[0])
+        destination = clean_content(col.children[1])
+        arrival     = clean_content(col.children[2]).to_i
 
-      memo << { line: line, destination: destination, arrival: arrival }
+        memo << { line: line, destination: destination, arrival: arrival }
+      end
     end
     self
   end
